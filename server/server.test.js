a4fd5e71ee21a7d9b22b3d13fb4f03156b1452ab
Kent Carmine <kentcarmine@gmail.com>
2017-12-27 16:36:01 -0600

    Added describe blocks to tests

--- server/server.test.js
+++ server/server.test.js
@@ -5,32 +5,40 @@ const expect = require('expect');
 
 let app = require('./server').app;
 
-it('should return Hello World response', (done) => {
-  request(app)
-    .get('/')
-    // .expect(200)
-    .expect(404)
-    // .expect('Hello world!')
-    // .expect({
-    //   error: 'Page not found.'
-    // })
-    .expect((res) => {
-      expect(res.body).toInclude({
-        error: "Page not found."
-      });
-    })
-    .end(done);
-});
+describe('Server', () => {
 
-it('should return correct user object', (done) => {
-  request(app)
-    .get('/users')
-    .expect(200)
-    .expect((res) => {
-      expect(res.body).toBeA('array').toInclude({
-        name: "Bob",
-        age: 30
-      });
-    })
-    .end(done);
+  describe('GET /', () => {
+    it('should return Hello World response', (done) => {
+      request(app)
+        .get('/')
+        // .expect(200)
+        .expect(404)
+        // .expect('Hello world!')
+        // .expect({
+        //   error: 'Page not found.'
+        // })
+        .expect((res) => {
+          expect(res.body).toInclude({
+            error: "Page not found."
+          });
+        })
+        .end(done);
+    });
+  });
+
+  describe("GET /users", () => {
+    it('should return correct user object', (done) => {
+      request(app)
+        .get('/users')
+        .expect(200)
+        .expect((res) => {
+          expect(res.body).toBeA('array').toInclude({
+            name: "Bob",
+            age: 30
+          });
+        })
+        .end(done);
+    });
+  });
+
 });
