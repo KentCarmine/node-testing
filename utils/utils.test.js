63e96dfc60d0ebd5e06f5b1b14b1974a529ee891
Kent Carmine <kentcarmine@gmail.com>
2017-12-27 16:03:19 -0600

    Added testing for async functions

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -14,3 +14,17 @@ it('should square a number', () => {
 
   expect(res).toBe(9).toBeA('number');
 });
+
+it('should async add two numbers', (done) => {
+  utils.asyncAdd(4, 3, (sum) => {
+    expect(sum).toBe(7).toBeA('number');
+    done();
+  });
+});
+
+it('should async square a number', (done) => {
+  utils.asyncSquare(5, (res) => {
+    expect(res).toBe(25).toBeA('number');
+    done();
+  });
+});
